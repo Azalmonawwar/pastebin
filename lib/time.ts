8c6521190520981de7c8a025c26c0c8cf85a30b0
azalmonawwwar <monawwarazal@gmail.com>
2026-01-28 21:07:45 +0530

    fixed bug

--- lib/time.ts
+++ lib/time.ts
@@ -1,10 +1,10 @@
 import { headers } from "next/headers";
 
-export function getCurrentTimeMs() {
+export async function getCurrentTimeMs() {
   const testMode = process.env.TEST_MODE === "1";
   const h = headers();
   if (testMode) {
-    const t = h.get("x-test-now-ms");
+    const t = (await h).get("x-test-now-ms");
     if (t) return Number(t);
   }
   return Date.now();
